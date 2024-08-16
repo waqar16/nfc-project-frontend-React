be5e6e22c05751b71f37b4018b9c8758ce40eb03
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-08-16 16:28:37 +0500

    .

--- src/components/accountActivate/activation.js
+++ src/components/accountActivate/activation.js
@@ -10,13 +10,12 @@ const Activation = () => {
   const { uid, token } = useParams();
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
-  
+  const [loading, setLoading] = useState(false);
+
 
   const handleActivation = async () => {
     try {
       setLoading(true);
-      // Activate user account
       const response = await axios.post('https://api.onesec.shop/auth/users/activation/', {
         uid,
         token,
@@ -26,18 +25,6 @@ const Activation = () => {
         setMessage('You have successfully activated your account.');
         toast.success('Account activated successfully. Please login.');
         navigate('/login')
-
-        // // Retrieve email and password from local storage
-        // const email = localStorage.getItem('email');
-        // const password = localStorage.getItem('password');
-
-        // // Login user
-        // const loginResponse = await axios.post('  https://api.onesec.shop/auth/token/login/', {
-        //   email,
-        //   password,
-        // });
-
-        // localStorage.setItem('authToken', loginResponse.data.auth_token);
       }
     } catch (error) {
       setLoading(false);
@@ -46,36 +33,7 @@ const Activation = () => {
       console.error('Activation error:', error);
     }
 
-    // finally{
-    //           // Fetch user data
-    //           const authToken = localStorage.getItem('authToken');
-    //           const userResponse = await axios.get('  https://api.onesec.shop/auth/users/me/', {
-    //             headers: {
-    //               Authorization: `Token ${authToken}`,
-    //             },
-    //           });
-
-    //           console.log(userResponse.data);
-      
-    //           // Create profile type
-    //           const profile = {
-    //             user: userResponse.data.id,
-    //             profile_type: localStorage.getItem('profile_type'),
-    //           }
-    //           await axios.post('  https://api.onesec.shop/api/profile_type/', profile, {
-    //             headers: {
-    //               Authorization: `Token ${authToken}`,
-    //             },
-    //           });
-
-    //           // Redirect based on profile type
-    //           if (localStorage.getItem('profile_type') === 'company') {
-    //             navigate('/company-profile');
-    //           } else {
-    //             navigate('/profile-summary');
-    //           }
-    // }
-    finally{
+    finally {
       setLoading(false);
     }
   };
@@ -91,7 +49,7 @@ const Activation = () => {
         </div>
         {message && <p style={{ color: message.includes('successfully') ? 'green' : 'red' }}>{message}</p>}
         <div>
-          {/* <Link to={'/'} className={styles.login__button}>Go to site</Link> */}
+
         </div>
       </form>
       {loading && <Loader />}
